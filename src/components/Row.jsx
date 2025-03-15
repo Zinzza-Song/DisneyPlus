ef169552891c0cda23be66c9b3d2e58b66033dd0
Han-Bin Song <thdgksq@naver.com>
2025-03-15 16:49:48 +0900

    모달 기능 추가 및 모달 CSS 작성

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,12 @@
 import { useCallback, useEffect, useState } from "react";
 import axios from "../api/axios";
 import "./Row.css";
+import MovieModal from "./MovieModal";
 
 const Row = ({ title, id, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelection] = useState({});
 
   const fetchMovieData = useCallback(async () => {
     const res = await axios.get(fetchUrl);
@@ -13,6 +16,11 @@ const Row = ({ title, id, fetchUrl }) => {
   useEffect(() => {
     fetchMovieData();
   }, [fetchMovieData]);
+
+  const handleClick = (movie) => {
+    setModalOpen(true);
+    setMovieSelection(movie);
+  };
 
   return (
     <div>
@@ -35,6 +43,7 @@ const Row = ({ title, id, fetchUrl }) => {
               className="row__poster"
               src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               alt={movie.name}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -49,6 +58,9 @@ const Row = ({ title, id, fetchUrl }) => {
           </span>
         </div>
       </div>
+      {modalOpen && (
+        <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+      )}
     </div>
   );
 };
