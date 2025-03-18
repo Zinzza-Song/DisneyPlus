63f7d3b456448ee2a0ea57c6c2861f6664f8dbfb
Han-Bin Song <thdgksq@naver.com>
2025-03-18 13:32:56 +0900

    구글 팝업 인증 기능 적용

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import styled from "styled-components";
 
 const Nav = () => {
@@ -7,6 +8,8 @@ const Nav = () => {
   const [searchValue, setSearchValue] = useState("");
   const { pathname } = useLocation();
   const navigate = useNavigate();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -26,6 +29,14 @@ const Nav = () => {
     navigate(`/search?q=${e.target.value}`);
   };
 
+  const handleAuth = () => {
+    signInWithPopup(auth, provider)
+      .then((res) => {})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <NavWraaper show={show}>
       <Logo>
@@ -37,7 +48,7 @@ const Nav = () => {
       </Logo>
 
       {pathname === "/" ? (
-        <Login>Login</Login>
+        <Login onClick={handleAuth}>Login</Login>
       ) : (
         <>
           <Input
--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD9moeUf_qsoyRbJZ2gKiTvpBZ6ThgEreQ",
   authDomain: "react-disney-plus-app-3004e.firebaseapp.com",
@@ -14,7 +10,6 @@ const firebaseConfig = {
   appId: "1:881104351319:web:cb0dbda48ef92fc54ef312",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
