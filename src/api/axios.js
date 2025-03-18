e6a221dca905904e60e73202f034caa21542a9d3
Han-Bin Song <thdgksq@naver.com>
2025-03-18 14:00:30 +0900

    API KEY 숨김처리

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "05f9d346ccdf978d062ab12b9aec165a",
+    api_key: import.meta.env.VITE_TMDB_API_KEY,
     language: "ko-KR",
   },
 });
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase/app";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD9moeUf_qsoyRbJZ2gKiTvpBZ6ThgEreQ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: "react-disney-plus-app-3004e.firebaseapp.com",
   projectId: "react-disney-plus-app-3004e",
   storageBucket: "react-disney-plus-app-3004e.firebasestorage.app",
